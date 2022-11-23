811c549826d664aa13266b75f4075fdf09704ea3
KainanJunior <118028222+KainanJunior@users.noreply.github.com>
2022-11-23 18:10:04 -0300

    up

--- Ramirez Guincho(GitHub)/assets/js/script.js
+++ Ramirez Guincho(GitHub)/assets/js/script.js
@@ -1,38 +1,39 @@
+
 var header = document.getElementById('header');
-    var navigationHeader = document.getElementById('navegacao-header');
-    var content = document.getElementById('content');
-    var showSidebar = false;
+var navigationHeader = document.getElementById('navegacao-header');
+var content = document.getElementById('content');
+var showSidebar = false;
 
-    function toggleSidebar()
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
     {
-        showSidebar = !showSidebar;
-        if(showSidebar)
-        {
-            navigationHeader.style.marginLeft = '-10vw';
-            navigationHeader.style.animationName = 'showSidebar';
-            content.style.filter = 'blur(2px)';
-        }
-        else
-        {
-            navigationHeader.style.marginLeft = '-100vw';
-            navigationHeader.style.animationName = '';
-            content.style.filter = '';
-        }
+        navigationHeader.style.marginLeft = '-15vw';
+        navigationHeader.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)';
     }
-
-    function closeSidebar()
+    else
     {
-        if(showSidebar)
-        {
-            showSidebar = true;
-            toggleSidebar();
-        }
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = '';
+        content.style.filter = '';
     }
+}
 
-    window.addEventListener('resize', function(event) {
-        if(window.innerWidth > 768 && showSidebar) 
-        {  
-            showSidebar = true;
-            toggleSidebar();
-        }
-    });
+function closeSidebar()
+{
+    if(showSidebar)
+    {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar) 
+    {  
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
